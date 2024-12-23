963633dd86b94892a86082743a3a0ffa1cde57ad
Sameer Gull <67495544+SAMEERGULL@users.noreply.github.com>
2024-12-23 17:37:26 +0500

    Inital commit

---
+++ learn-pinia/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="count">
+      0
+    </div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.count {
+  font-size: 70px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 30px;
+  margin: 10px;
+}
+</style>
